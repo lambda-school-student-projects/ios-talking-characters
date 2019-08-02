2ab3cc7cb8f7d165629b3276b53f8c80ed78db57
John Kouris <jdkouris@gmail.com>
2019-08-01 18:07:54 -0700

    Created base project

---
+++ TalkingCharacters/TalkingCharacters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TalkingCharacters
+//
+//  Created by John Kouris on 8/1/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
