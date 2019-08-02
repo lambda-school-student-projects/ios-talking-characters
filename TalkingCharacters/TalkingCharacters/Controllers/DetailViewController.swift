4153750516b98a54bda5ccab15f1ec56afab206e
John Kouris <john.kouris@slalom.com>
2019-08-02 16:34:46 -0700

    Completed stretch goal: added segmentedControl for user to pick the fps between 10, 15, and 30

--- TalkingCharacters/TalkingCharacters/Controllers/DetailViewController.swift
+++ TalkingCharacters/TalkingCharacters/Controllers/DetailViewController.swift
@@ -12,17 +12,47 @@ class DetailViewController: UIViewController {
     
     @IBOutlet var characterNameLabel: UILabel!
     @IBOutlet var characterImage: UIImageView!
+    @IBOutlet var segmentedControl: UISegmentedControl!
     
     var character: Model.Animation?
 
     override func viewWillAppear(_ animated: Bool) {
+        runAnimation()
+    }
+    
+    @IBAction func indexChanged(_ sender: Any) {
         
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            runAnimation()
+        case 1:
+            runAnimation()
+        case 2:
+            runAnimation()
+        default:
+            runAnimation()
+        }
+        
+    }
+    
+    func runAnimation() {
         guard let character = character else { return }
         characterNameLabel.text = character.rawValue
         let cells = Model.shared.cells(for: character)
         characterImage.animationImages = cells
         characterImage.animationRepeatCount = 0
-        characterImage.animationDuration = Double(Model.shared.cells(for: character).count) / 15.0
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            characterImage.animationDuration = Double(Model.shared.cells(for: character).count) / 10.0
+        case 1:
+            characterImage.animationDuration = Double(Model.shared.cells(for: character).count) / 15.0
+        case 2:
+            characterImage.animationDuration = Double(Model.shared.cells(for: character).count) / 30.0
+        default:
+            characterImage.animationDuration = Double(Model.shared.cells(for: character).count) / 30.0
+        }
+        
         characterImage.startAnimating()
     }
 }
