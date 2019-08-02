15b3b3f9729ebd9ecb0e62a2e096255a1a1284c4
Vici Shaweddy <vshaweddy@gmail.com>
2019-08-01 22:52:03 -0400

    adding functions

--- TalkingCharacters/TalkingCharacters/Models/Model.swift
+++ TalkingCharacters/TalkingCharacters/Models/Model.swift
@@ -6,4 +6,53 @@
 //  Copyright © 2019 Vici Shaweddy. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension Int {
+    fileprivate func stringPadded(to count: Int) -> String {
+        var string = String(self)
+        while string.count < count { string = "0" + string}
+        return string
+    }
+}
+
+class Model {
+    static let shared = Model()
+    private init() {}
+    enum Animation: String, CaseIterable {
+        case bureaucrat = "Talking Bureaucrat"
+        case frog = "Talking Frog"
+        case ranger = "Talking Ranger"
+        case vendor = "Talking Vendor"
+        case bot = "Talking Woodbot"
+    }
+    
+    private let cellCounts = [Animation.bureaucrat: 26, .frog: 62, .ranger: 86, .vendor: 36, .bot: 69]
+    
+    var characters: [Animation] {
+        return Animation.allCases
+    }
+    
+    func image(for character: Animation) -> UIImage {
+        return UIImage(named: character.rawValue + "000") ?? UIImage()
+    }
+    
+    func cells(for character: Animation) -> [UIImage] {
+        var images: [UIImage] = []
+        
+        guard let numberOfImages = cellCounts[character] else { return [] }
+        
+        for number in 0...numberOfImages {
+            // getting the image number
+            let imageNumber = number.stringPadded(to: 3)
+            
+            // getting the character name
+            let characterName = character.rawValue + imageNumber
+            guard let image = UIImage(named: characterName) else { return [] }
+            
+            // add to images array
+            images.append(image)
+        }
+        return images
+    }
+}
