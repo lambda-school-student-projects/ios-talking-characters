24d33c00628d93c393230972ea50941dce68dbc0
Vici Shaweddy <vshaweddy@gmail.com>
2019-08-02 15:28:51 -0400

    adding segmented control

--- TalkingCharacters/TalkingCharacters/View Controllers/DetailViewController.swift
+++ TalkingCharacters/TalkingCharacters/View Controllers/DetailViewController.swift
@@ -12,26 +12,48 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var characterLabel: UILabel!
     @IBOutlet weak var characterImageView: UIImageView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     var character: Model.Animation?
 
+    var cells: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        
+        animateCharacter(at: 10)
+        
+    }
+    
+    func animateCharacter(at duration: Double) {
         // character name
         guard let character = character else { return }
         characterLabel.text = character.rawValue
         
-        let cells = Model.shared.cells(for: character)
+        cells = Model.shared.cells(for: character)
         characterImageView.animationImages = cells
         characterImageView.animationRepeatCount = 0
-        characterImageView.animationDuration = TimeInterval(cells.count / 15)
+        characterImageView.animationDuration = TimeInterval(cells.count / Int(duration))
         characterImageView.startAnimating()
     }
     
-
+    @IBAction func indexChanged(_ sender: Any) {
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            animateCharacter(at: 10)
+        case 1:
+            animateCharacter(at: 15)
+        case 2:
+            animateCharacter(at: 30)
+        default:
+            characterImageView.animationDuration = TimeInterval(cells.count / 100)
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
