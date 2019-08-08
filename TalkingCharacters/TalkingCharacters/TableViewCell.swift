e22a9397ffef9afa1c1b0744a5d367429e8f70c4
JosephRogers94 <Joerogersss2016@gmail.com>
2019-08-08 13:27:37 -0500

    IB Setup

---
+++ TalkingCharacters/TalkingCharacters/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TalkingCharacters
+//
+//  Created by Joseph Rogers on 8/8/19.
+//  Copyright © 2019 Joe Rogers. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
