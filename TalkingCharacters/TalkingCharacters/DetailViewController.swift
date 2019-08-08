896b48f3ea4b483481fc3be27f313164c292546c
JosephRogers94 <Joerogersss2016@gmail.com>
2019-08-08 14:10:20 -0500

    MVP

--- TalkingCharacters/TalkingCharacters/DetailViewController.swift
+++ TalkingCharacters/TalkingCharacters/DetailViewController.swift
@@ -12,22 +12,21 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var characterNameLabel: UILabel!
     @IBOutlet weak var detailImageView: UIImageView!
-    
+    var character: Model.Animation?
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        guard let character = character else { return }
+        characterNameLabel.text = character.rawValue
+        let cells = Model.shared.cells(for: character)
+        detailImageView.animationImages = cells
+        detailImageView.animationRepeatCount = 0
+        detailImageView.animationDuration = TimeInterval(cells.count / 15)
+        detailImageView.startAnimating()
     }
-    */
-
+ 
 }
--- TalkingCharacters/TalkingCharacters/TableViewController.swift
+++ TalkingCharacters/TalkingCharacters/TableViewController.swift
@@ -24,67 +24,31 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return Model.shared.characters.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CharCell", for: indexPath)
-
-        // Configure the cell...
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CharCell", for: indexPath) as? TableViewCell else { return UITableViewCell() }
+        cell.cellLabelImage.image = Model.shared.image(for: Model.shared.characters[indexPath.row])
         return cell
     }
  
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "DetailSegue" {
+            guard let vc = segue.destination as? DetailViewController,
+                let indexPath = tableView.indexPathForSelectedRow
+                else { return }
+            
+            vc.character = Model.shared.characters[indexPath.row]
+        }
     }
-    */
-
 }
